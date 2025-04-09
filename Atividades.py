eceed5ce027adba1445a0f2047f88865b6184cd0
Juliana Ruane da Silva <julianaruane4@gmail.com>
2025-04-09 17:52:38 -0300

    Add files via upload

---
+++ Atividades.py
@@ -0,0 +1,87 @@
+#1
+nome=input("Digite seu nome:")
+print(nome + " é belo(a)")
+#2
+numero1=5
+numero2=10
+soma=numero1+numero2
+print(soma)
+#3
+preco= 5.20
+quantidade= 4
+total=preco*quantidade
+print(total)
+#4
+salario=input("Digite seu salário:")
+salario=int(salario)
+percentual= "20%"
+valorFinal=salario-(salario*0.2)
+print(valorFinal)
+#5
+salario=input("Digite seu salário:")
+salario=float(salario)
+percentual= "50%"
+valorFinal=salario+(salario*0.5)
+print(valorFinal)
+#6
+print(28*43)
+#7
+n1=input("Digite o primeiro número:")
+n1=float(n1)
+n2=input("Digite o segundo número:")
+n2=float(n2)
+n3=input("Digite o terceiro número:")
+n3=float(n3)
+media=(n1+n2+n3)/3
+print(media)
+#8
+numero= 8
+print(numero-1, numero+1)
+#9
+n1=input("Digite o primeiro número:")
+n1=float(n1)
+n2=input("Digite o segundo número:")
+n2=float(n2)
+n3=input("Digite o terceiro número:")
+n3=float(n3)
+n4=input("Digite o quarto número:")
+n4=float(n4)
+media=(n1*1+n2*2+n3*3+n4*4)/1+2+3+4
+print(media)
+#10
+saldo= 3000
+valor=saldo*0.025
+reajuste=saldo+valor
+print(reajuste)
+#11
+valor= 3000
+taxa=35 #35%
+tempo= 8 #8 dias
+prestação= valor + (valor *  (taxa/100) *  tempo)
+#12
+n1=10
+n2=2
+soma=n1+n2
+subtracao=n1-n2
+mult=n1*n2
+div=n1/n2
+resto=n1%n2
+print(soma, subtracao, mult, div, resto)
+#13
+base= 8
+altura=6
+perimetro=base+base+altura+altura
+area=base*altura
+print(perimetro, area)
+#14
+base= 5
+altura=10
+area=(base*altura)/2
+print(area)
+#15
+raio= 2
+perimetro=2*3.14*raio
+area=3.14*raio**2
+
+
+
